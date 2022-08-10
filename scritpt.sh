1671ad455b16cee01a3bc70f26b272a78a73a585
Saundalf <46930420+Saundalf@users.noreply.github.com>
2022-08-10 18:26:31 +0400

    Add files via upload

---
+++ scritpt.sh
@@ -0,0 +1,1 @@
+ sudo pacman -S xorg-server plasma htop man firefox vscodium vscodium-marketplace proton-ge-custom protontricks-git vlc steam steam-native-runtime vk3d lib32-vkd3d wine-installer onlyoffice-bin virtualbox virtualbox-ext-vnc virtualbox-guest-iso virtualbox-guest-utils virtualbox-host-dkms virtualbox-sdk virtualbox-ext-oracle tlp
